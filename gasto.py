47e368518bedcc7faaa9b11dcf8466c4d3e2ee46
PeterArgueta <peterarguedo@gmail.com>
2023-03-16 10:38:41 -0600

    modified:   gasto.py

--- gasto.py
+++ gasto.py
@@ -13,6 +13,10 @@ galones= df['Galones']
 
 gasto_mensual= df.groupby('Mes')['Gasto'].sum()
 
+gasto_total=df['Gasto'].sum()
+
+#print(gasto_total)
+
 mi_por_galon = kilometros / galones
 
 fig, axs = plt.subplots(nrows=2, figsize=(10, 6))
@@ -27,7 +31,6 @@ axs[1].set_ylabel('Gasto mensual (Q)')
 axs[1].set_xlabel(None)
 axs[1].set_title('Gasto mensual de combustible')
 axs[1].grid(True)
-
 
 plt.tight_layout()
 plt.show()
