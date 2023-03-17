7a6ba865b0f6eb5d02f9e12663856e3d23e78caa
PeterArgueta <peterarguedo@gmail.com>
2023-03-17 10:43:53 -0600

    actualizacion

--- gasto.py
+++ gasto.py
@@ -1,18 +1,19 @@
+"""Progama para llevar un control del consumo y gasto de tu vehiculo"""
+
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Cargar el archivo CSV
+"""Cargar el archivo CSV"""
 df = pd.read_csv('gasolina.csv')
 
-# Seleccionar las columnas de fecha y kilómetros
+"""Seleccionar las columnas de fecha y kilómetros"""
+
 fechas = pd.to_datetime(df['Fecha'])
 df['Fecha'] = pd.to_datetime(df['Fecha'])
 df['Mes'] = df['Fecha'].dt.to_period('M')
 kilometros = df['Millas']
 galones= df['Galones']
-
 gasto_mensual= df.groupby('Mes')['Gasto'].sum()
-
 gasto_total=df['Gasto'].sum()
 
 #print(gasto_total)
@@ -26,6 +27,7 @@ axs[0].set_ylabel('MPG')
 axs[0].set_title('Consumo de Gasolina')
 axs[0].grid(True)
 
+"""Gráfica de gasto mensual"""
 gasto_mensual.plot(kind='bar', ax=axs[1])
 axs[1].set_ylabel('Gasto mensual (Q)')
 axs[1].set_xlabel(None)
@@ -34,4 +36,5 @@ axs[1].grid(True)
 
 plt.tight_layout()
 plt.show()
+
 
